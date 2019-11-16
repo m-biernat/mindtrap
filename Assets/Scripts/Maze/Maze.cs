8ad66588cf9da3c6e242a7a98b6ec979ee765ef0
m-bt <michalbiernat@windowslive.com>
2019-11-16 19:30:51 +0100

    Update 16-11-19 #2
    Added Maze and MazeGenerator classes and set up basic MazeTest scene.

---
+++ Assets/Scripts/Maze/Maze.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public class Maze
+{
+    private int width, height;
+
+    public readonly int cols, rows;
+    public readonly int[,] grid;
+
+    private readonly int x = 1, y = 0;
+    public static readonly int wall = 0, empty = 1, visited = 2;
+
+    private int initialX, initialY;
+    private int[] curr, next;
+
+    private Stack<int[]> availableNeighbors;
+
+    public Maze(int width = 11, int height = 7, int initialX = -1, int initialY = 1, int seed = 10)
+    {
+        this.width = width;
+        this.height = height;
+        this.initialX = initialX;
+        this.initialY = initialY;
+
+        Random.InitState(seed);
+        availableNeighbors = new Stack<int[]>();
+
+        cols = (2 * width) - 1;
+        rows = (2 * height) + 1;
+
+        grid = new int[rows, cols];
+
+        Generate();
+    }
+
+    private void Generate()
+    {
+        if (width % 2 == 0 || height % 2 == 0) return;
+
+        GridInit();
+
+        SetInitialCell();
+
+        curr = new int[] { initialY, initialX };
+        next = curr;
+
+        do
+        {
+            while (next[x] != -1)
+            {
+                grid[curr[y], curr[x]] = visited;
+                SelectNeighbor();
+                RemoveWall();
+
+                curr = next;
+            }
+
+            curr = availableNeighbors.Pop();
+            next = curr;
+        }
+        while (availableNeighbors.Count != 0);
+
+        grid[0, initialX] = visited;
+        grid[rows - 1, initialX] = visited;
+    }
+
+    private void GridInit()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (i % 2 == 1 && j % 2 == 0)
+                {
+                    grid[i, j] = empty;
+                }
+                else
+                {
+                    grid[i, j] = wall;
+                }
+            }
+        }
+    }
+
+    private void SetInitialCell()
+    {
+        if (initialX == -1)
+        {
+            initialX = width - 1;
+        }
+
+        if (initialY == -1)
+        {
+            initialY = height - 1;
+        }
+    }
+
+    private void SelectNeighbor()
+    {
+        List<int[]> neighbors = new List<int[]>();
+
+        // North of current (x, y-2)
+        if (curr[y] - 2 >= 0 && grid[curr[y] - 2, curr[x]] == empty)
+        {
+            neighbors.Add(new int[] { curr[y] - 2, curr[x] });
+        }
+
+        // East of current (x+2, y)
+        if (curr[x] + 2 < grid.GetLength(x) && grid[curr[y], curr[x] + 2] == empty)
+        {
+            neighbors.Add(new int[] { curr[y], curr[x] + 2 });
+        }
+
+        // South of current (x, y+2)
+        if (curr[y] + 2 < grid.GetLength(y) && grid[curr[y] + 2, curr[x]] == empty)
+        {
+            neighbors.Add(new int[] { curr[y] + 2, curr[x] });
+        }
+
+        // West of current (x-2, y)
+        if (curr[x] - 2 >= 0 && grid[curr[y], curr[x] - 2] == empty)
+        {
+            neighbors.Add(new int[] { curr[y], curr[x] - 2 });
+        }
+
+        if (neighbors.Count > 0)
+        {
+            next = neighbors[Random.Range(0, neighbors.Count)];
+
+            if (neighbors.Count > 1)
+            {
+                availableNeighbors.Push(curr);
+            }
+        }
+        else
+        {
+            next[x] = -1;
+        }
+    }
+
+    private void RemoveWall()
+    {
+        if (next[x] != -1)
+        {
+            if (curr[x] == next[x])
+            {
+                int yIndex = (curr[y] + next[y]) / 2;
+                grid[yIndex, curr[x]] = visited;
+                return;
+            }
+
+            if (curr[y] == next[y])
+            {
+                int xIndex = (curr[x] + next[x]) / 2;
+                grid[curr[y], xIndex] = visited;
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Maze/MazeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public GameObject mazePrefab;
+    public Transform anchor;
+
+    [Space]
+    public int width = 11;
+    public int height = 7;
+
+    public int initialX = -1, initialY = 1;
+
+    public int seed = 10;
+
+    [Space]
+    public int positionOffset = 3;
+
+    void Start()
+    {
+        Maze maze = new Maze(width, height, initialX, initialY, seed);
+
+        for (int i = 0; i < maze.rows; i++)
+        {
+            for (int j = 0; j < maze.cols; j++)
+            {
+                if (maze.grid[i, j] == Maze.visited)
+                {
+                    Vector3 position = new Vector3(positionOffset * j, 0f, 
+                                                   positionOffset * i);
+
+                    Instantiate(mazePrefab, position + anchor.position, 
+                                mazePrefab.transform.rotation, transform);
+                }
+            }
+        }
+    }
+}
